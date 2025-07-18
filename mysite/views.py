7b5e27fc333ab8412b87c257d35c3d77abf38cc7
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-07-18 16:58:59 +0530

    fetched all employees from database

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from employees.models import Employee
 
 def home(request):
-    return render(request, 'home.html')
+    employess = Employee.objects.all()  # Fetch all employees from the database
+    context = {
+        'employees': employess, # Pass the employees to the template context
+    }
+    return render(request, 'home.html', context)  # Render the home template with the context
