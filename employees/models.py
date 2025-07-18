3e329aece000362764f058ecac5d800f9fc89240
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-07-18 16:54:59 +0530

    feat:created fields in the table

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100,unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.designation}"
