eed8a3d101d4e54d5afe4115142ec5b23e1e283b
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-07-19 18:04:52 +0530

    feat:added context to employee detail

--- employees/views.py
+++ employees/views.py
@@ -1,7 +1,10 @@
 from employees.models import Employee
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee, pk=pk)  # Fetch the employee by primary key
-    return HttpResponse(f"Employee: {employee.first_name} {employee.last_name}, Designation: {employee.designation}")  # Return a simple response with employee details
+    context = {
+        'employee' : employee,
+    }
+    return render(request, 'employee_detail.html',context)  # Render the employee detail template with the employee data
